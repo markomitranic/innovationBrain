51c510e7f169da66dee7fec5e530c5364d708861
Nikola Milenkovic <nikola.milenkovic@blacksheep.co.rs>
2016-01-27 16:55:02 +0100

    latest articles

--- assets/js/partials/functions.js
+++ assets/js/partials/functions.js
@@ -1,10 +1,5 @@
 // This function only echoes 'Hello World!' into console if called.
 function consoleTest() {
-	console.log('Hello Wordld!');
+	console.log('Hello World!');
 }
 
-$('#hero').blurjs({
-	source: '#headerWrapper',
-	radius: 7,
-	overlay: 'rgba(255,255,255,0.4)'
-});
