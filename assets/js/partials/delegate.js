2070957d132bb6945c7c1b04d301bc06627ff7ca
markomitranic <marko.mitranic@me.com>
2016-01-27 03:02:38 +0100

    Initial commit. Set up gulp and basic structure

---
+++ assets/js/partials/delegate.js
@@ -0,0 +1,4 @@
+//A test JS function just to check if everything works as planned
+
+consoleTest();
+
---
+++ assets/js/partials/functions.js
@@ -0,0 +1,4 @@
+// This function only echoes 'Hello World!' into console if called.
+function consoleTest() {
+	console.log('Hello World!');
+}
